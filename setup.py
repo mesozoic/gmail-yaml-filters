3a4dac7bbd8ca07a92139a3c84d18b2c36cfe179
Alex Levy <levy@yelp.com>
2015-10-29 14:39:15 -0700

    More metadata for setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
+from setuptools import find_packages
 from setuptools import setup
 
 
 setup(
     name='gmail-yaml-filters',
+    description='A quick tool for generating Gmail filters from YAML rules.',
+    url='https://github.com/aclevy/gmail-yaml-filters',
+    version='0.1',
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+    packages=find_packages('.'),
     entry_points={
         'console_scripts': [
             'gmail-yaml-filters = gmail_yaml_filters.main:main',
