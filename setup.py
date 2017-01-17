790d27abc51daca38a2e7ce1f2cd2e6e1e64ca41
Alex Levy <mesozoic@users.noreply.github.com>
2017-01-16 16:47:04 -0800

    v0.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email='mesozoic@users.noreply.github.com',
     description='A quick tool for generating Gmail filters from YAML rules.',
     url='https://github.com/mesozoic/gmail-yaml-filters',
-    version='0.3',
+    version='0.4',
     classifiers=[
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
